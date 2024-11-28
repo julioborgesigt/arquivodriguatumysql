8d3f32e07322a6038a27940465f97a85624a1980
julioborgesigt <julio.aparecido3@email.com>
2024-11-28 07:37:01 -0300

    json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
-const port = 3000;
+const port = 3306;
 
 // Configuração do banco de dados
 const sequelize = new Sequelize('yhoaen_banco', 'yhoaen_banco', 'tSpoSSxdDr', {
